c0c86405d6b306fcc781afe85d3506148d9ec898
Patrick Mulder <mulder.patrick@gmail.com>
2013-08-27 21:05:58 +0200

    add proxy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,25 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+  var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
+
   require('matchdep').filterAll('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Check if config.json exists
+    if (grunt.file.exists('config.json')) {
+      var config = grunt.file.readJSON("config.json");
+    }
+    else {
+      grunt.log.error('Wow, wow, wow! Please, have a `config.json` for fuck\'s sake!');
+      process.exit(1);
+    }
 
   // Project configuration.
   grunt.initConfig({
 
 
     pkg: grunt.file.readJSON('package.json'),
+
 
     jshint: {
       options: {
@@ -38,13 +50,14 @@ module.exports = function(grunt) {
       server: {
         options: {
           port: 8000,
-          base: 'dist',
+          base: 'public',
+          logger: 'dev',
           hostname: 'localhost'
         }
       },
       proxies: [
                 {
-                    context: '/cortex',
+                    context: '/context',
                     host: '0.0.0.0',
                     port: 9292,
                     https: false,
@@ -57,8 +70,9 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-connect-proxy');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'connect:server', 'watch']);
+  grunt.registerTask('default', ['jshint', 'connect:server', 'configureProxies', 'watch']);
 
 };
